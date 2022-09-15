74a4aeab1ce6568a20cd867944e5e480a9d9705e
Fernando Warno <ferwar@hotmail.com>
2022-09-15 08:27:05 -0300

    Inicio formulario html

--- practica02.js
+++ practica02.js
@@ -1,1 +1,4 @@
-// https://www.youtube.com/watch?v=s3pC93LgP18
+// Parte1: https://www.youtube.com/watch?v=cEKDyzoTXb4
+// Parte2: https://www.youtube.com/watch?v=s3pC93LgP18
+// Repositorio : https://github.com/falconmasters/formulario-css-grid/tree/codigo_base
+
